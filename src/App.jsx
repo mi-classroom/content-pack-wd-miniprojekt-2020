3c83f9a55de84cd8b42b750be247de9384262475
Steytz <lucianofreelancingde@gmail.com>
2020-08-03 16:36:17 +0200

    Add sample App Component and scss file

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.scss'
+
+const App = () => (
+    <div className="container">
+        <h1>Hello World, React!11</h1>
+    </div>
+)
+
+export default App;
